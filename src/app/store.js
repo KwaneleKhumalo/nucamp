3554ada547873865eb36a2433d6acdce1c4ac0df
Kwanele <kwanele@Kwaneles-MacBook-Pro.local>
2023-05-12 19:41:25 -0600

    logger and dispatch with redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { campSiteReducer } from '../features/campsites/campsitesSlice';
+import { commentsReducer } from "../features/comments/commentsSlice"
+import { partnersReducer } from "../features/partners/partnerSlice"
+import { promotionsReducer } from "../features/promotions/promotionsSlice"
 
 export const store = configureStore({
   reducer: {
     campsites: campSiteReducer,
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer
   },
-});
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
+})
+
